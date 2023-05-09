aaf493a9c47b9e27becbd09015bde3c97e26ecf3
George Dang <53052793+gtdang@users.noreply.github.com>
2023-05-09 14:37:04 -0400

    refactor: post generation hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -6,25 +6,34 @@ import sys
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
 
-def remove_file(filepath):
-    os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
-
-
-def remove_dir(dir_path):
-    try:
-        shutil.rmtree(dir_path)
-    except OSError as e:
-        print("Error: %s : %s" % (dir_path, e.strerror))
+def remove_path(path):
+    path = path.strip()
+    if path and os.path.exists(path):
+        # if directory else file
+        if os.path.isdir(path):
+            shutil.rmtree(path)
+        else:
+            os.remove(os.path.join(PROJECT_DIRECTORY, path))
 
 
 if __name__ == '__main__':
 
     if 'experimentalist' in '{{ cookiecutter.autora_contribution_type|lower }}':
-        remove_dir('src/autora/theorist')
-        remove_file('tests/test_theorist_example.py')
+        list_remove = ['src/autora/theorist',
+                       'tests/test_theorist_example.py',
+                       ]
 
     if 'theorist' in '{{ cookiecutter.autora_contribution_type|lower }}':
-        remove_dir('src/autora/experimentalist')
-        remove_file('tests/test_experimentalist_pooler_example.py')
-        remove_file('tests/test_experimentalist_sampler_example.py')
+        list_remove = ['src/autora/experimentalist',
+                       'tests/test_experimentalist_pooler_example.py',
+                       'tests/test_experimentalist_sampler_example.py',
+                       ]
+
+    for path_remove in list_remove:
+        try:
+            remove_path(path_remove)
+        except OSError as e:
+            print(f"Error: {path_remove} : {e.strerror}")
+            sys.exit(1)
+
 
