d8cacf50c09c8fe35577b8ed53af6cd108de5e4d
George Dang <53052793+gtdang@users.noreply.github.com>
2023-05-10 16:32:24 -0400

    chore: add additional init files to version control

---
+++ {{ cookiecutter.__project_slug }}/src/autora/experiment_runner/recruitment_manager/{{ cookiecutter.__python_name }}/__init__.py
@@ -0,0 +1,48 @@
+"""
+Example recruitment manager:
+    These are example functions that could be included 
+    (but are not necessarily included)
+"""
+
+
+def example_setup_recruitment(args):
+    """
+    Add your description here.
+    Set up a study on an example recruitment platform.
+    
+    Args:
+        the arguments needed to set up the study (e.g., credentials)
+    """
+    pass
+
+
+def start_recruitment(args):
+    """
+    Add description here.
+    Start the recruitment on an example recruitment platform.
+
+    Args:
+        the arguments needed to start the recruitment (e.g., credentials)
+    """
+    pass
+
+
+def pause_recruitment(args):
+    """
+    Add description here.
+    Pause the recruitment on an example recruitment platform.
+
+    Args:
+        the arguments needed to start the recruitment (e.g., credentials)
+    """
+
+
+def check_recruitment_status(args):
+    """
+    Add description here.
+    Get information on the status of recruitment for a study on an example recruitment platform.
+    
+    Args:
+        the arguments needed to start the recruitment (e.g., credentials)
+    """
+    pass
