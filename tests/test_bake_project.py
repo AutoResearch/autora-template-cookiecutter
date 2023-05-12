990aa2fa70320102820130ad4e59d9e921e2a190
George Dang <53052793+gtdang@users.noreply.github.com>
2023-05-12 10:31:01 -0400

    test: defaults test

---
+++ tests/test_bake_project.py
@@ -0,0 +1,84 @@
+from contextlib import contextmanager
+import shlex
+import os
+import sys
+import subprocess
+import yaml
+import datetime
+import pytest
+from cookiecutter.utils import rmtree
+
+from click.testing import CliRunner
+
+import importlib
+
+
+@contextmanager
+def inside_dir(dirpath):
+    """
+    Execute code from inside the given directory
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    old_path = os.getcwd()
+    try:
+        os.chdir(dirpath)
+        yield
+    finally:
+        os.chdir(old_path)
+
+
+@contextmanager
+def bake_in_temp_dir(cookies, *args, **kwargs):
+    """
+    Delete the temporal directory that is created when executing the tests
+    :param cookies: pytest_cookies.Cookies,
+        cookie to be baked and its temporal files will be removed
+    """
+    result = cookies.bake(*args, **kwargs)
+    try:
+        yield result
+    finally:
+        rmtree(str(result.project))
+
+
+def run_inside_dir(command, dirpath):
+    """
+    Run a command from inside a given directory, returning the exit status
+    :param command: Command that will be executed
+    :param dirpath: String, path of the directory the command is being run.
+    """
+    with inside_dir(dirpath):
+        return subprocess.check_call(shlex.split(command))
+
+def check_output_inside_dir(command, dirpath):
+    "Run a command from inside a given directory, returning the command output"
+    with inside_dir(dirpath):
+        return subprocess.check_output(shlex.split(command))
+
+
+def project_info(result):
+    """Get toplevel dir, project_slug, and project dir from baked cookies"""
+    project_path = str(result.project)
+    project_slug = os.path.split(project_path)[-1]
+    project_dir = os.path.join(project_path, project_slug)
+    return project_path, project_slug, project_dir
+
+
+def test_bake_with_defaults(cookies):
+    with bake_in_temp_dir(cookies, extra_context={"contribution_name": "test"}) as result:
+        assert result.project.isdir()
+        assert result.exit_code == 0
+        assert result.exception is None
+
+        found_toplevel_files = [f.basename for f in result.project.listdir()]
+        assert 'mkdocs.yml' in found_toplevel_files
+        assert '.pre-commit-config.yaml' in found_toplevel_files
+        assert 'pyproject.toml' in found_toplevel_files
+        assert 'README.md' in found_toplevel_files
+        assert '.gitignore' in found_toplevel_files
+        assert '.github' in found_toplevel_files
+        assert 'tests' in found_toplevel_files
+        assert 'docs' in found_toplevel_files
+        assert 'mkdocs' in found_toplevel_files
+        assert 'src' in found_toplevel_files
+
