c236c81310ce610e87165eefcc03b8ed79dbda30
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-05-28 18:24:01 -0400

    chore: generating all contributions and merging them, generating README

--- generate_full/generate.py
+++ generate_full/generate.py
@@ -2,8 +2,6 @@ import os
 from cookiecutter.main import cookiecutter
 import shutil
 
-
-
 DIR = os.path.dirname(os.path.realpath(__file__))
 PARENT = os.path.join(DIR, '..')
 FOLDER_NAME = "target"
@@ -21,7 +19,7 @@ def bake_contribution(contrib_type, sub_type=None):
     if sub_type is not None:
         name = f"{contrib_type}-{sub_type}-example"
     else:
-        name =  f"{contrib_type}-example"
+        name = f"{contrib_type}-example"
     extra_content = {
         "__contrib_name": name,
         "__autora_contribution_type": contrib_type,
@@ -33,6 +31,7 @@ def bake_contribution(contrib_type, sub_type=None):
         no_input=True,
         extra_context=extra_content,
         output_dir=FOLDER_PATH)
+
 
 def copy_new_files(src, dst):
     """Copy files from src to dst, only if they don't exist in dst."""
@@ -46,6 +45,28 @@ def copy_new_files(src, dst):
     else:
         if not os.path.exists(dst):
             shutil.copy2(src, dst)
+
+
+def create_readme():
+    readme_src = os.path.join(PARENT, '{{ cookiecutter.__project_slug }}/README.md')
+    readme_dist = os.path.join(FOLDER_PATH, 'README.md')
+
+    shutil.copy(readme_src, readme_dist)
+
+    # rempove lines starting with {%
+    output_lines = []
+    with open(readme_dist, "r") as file:
+        for line in file:
+            # Skip lines that start with "{%"
+            if line.lstrip().startswith("{%"):
+                continue
+            output_lines.append(line)
+
+    # Write the output lines back to the README
+    with open(readme_dist, "w") as file:
+        for line in output_lines:
+            file.write(line)
+
 
 def main():
     # create all contributions
@@ -62,14 +83,14 @@ def main():
     # get a list of all created folders
     contribution_folders = [name for name in os.listdir(FOLDER_PATH) if os.path.isdir(os.path.join(FOLDER_PATH, name))]
 
-
-    # make autora
-    os.makedirs(os.path.join(FOLDER_PATH, '.'), exist_ok=True)
-
     # copy all folders into the autora folder (if they don't exist)
     for f in contribution_folders:
-        copy_new_files(os.path.join(FOLDER_PATH, f), os.path.join(FOLDER_PATH, '.'))
-        shutil.rmtree(os.path.join(FOLDER_PATH, f))
+        src_folder = os.path.join(FOLDER_PATH, f)
+        copy_new_files(src_folder, os.path.join(FOLDER_PATH, '.'))
+        shutil.rmtree(src_folder)
+
+    create_readme()
+
 
 if __name__ == '__main__':
     main()
