5267df6b4bf6917e777c715cac91b7c9bf6c8bcb
Younes Strittmatter <younes_strittmatter@brown.edu>
2023-06-16 15:15:25 -0400

    chore: add synthetic as subtype of experiment_runner

--- generate_full/generate.py
+++ generate_full/generate.py
@@ -12,7 +12,7 @@ os.makedirs(FOLDER_PATH, exist_ok=True)
 
 contribution_types = ["theorist", "experimentalist", "experiment_runner"]
 experimentalist_subtype = ["sampler", "pooler"]
-runner_subtypes = ["experimentation_manager", "recruitment_manager"]
+runner_subtypes = ["experimentation_manager", "recruitment_manager", "synthetic"]
 
 
 def bake_contribution(contrib_type, sub_type=None):
